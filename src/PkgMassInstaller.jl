dc19546377ec3eef05407ea65508a85b83c64286
josePereiro <jose.pereiro.lab@gmail.com>
2021-03-03 18:04:23 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/PkgMassInstaller.jl
@@ -0,0 +1,5 @@
+module PkgMassInstaller
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PkgMassInstaller
+using Test
+
+@testset "PkgMassInstaller.jl" begin
+    # Write your tests here.
+end
