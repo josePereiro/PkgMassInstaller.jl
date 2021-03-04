6b853a3f4a26b705eb84bbbeaa8a608d7ea8fcca
josePereiro <jose.pereiro.lab@gmail.com>
2021-03-03 19:54:48 -0500

    WIP, CI, up to v0.2.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,31 +3,29 @@ using Test
 
 @testset "PkgMassInstaller.jl" begin
 
-    # @test true
+    JULIA_CMD = Base.julia_cmd()
+    ROOT_DIR = pathof(PkgMassInstaller) |> dirname |> dirname
+    PROJ_DIR = joinpath(ROOT_DIR, "Project.toml")
+    SCRIPT_FILE = joinpath(ROOT_DIR, "scripts", "install.jl")
+    @assert isfile(SCRIPT_FILE)
 
-    # JULIA_CMD = Base.julia_cmd()
-    # ROOT_DIR = pathof(PkgMassInstaller) |> dirname |> dirname
-    # PROJ_DIR = joinpath(ROOT_DIR, "Project.toml")
-    # SCRIPT_FILE = joinpath(ROOT_DIR, "scripts", "install.jl")
-    # @assert isfile(SCRIPT_FILE)
-
-    # @info("Dry Run, -deep = 0")
-    # run(`$JULIA_CMD --project=$PROJ_DIR -- $SCRIPT_FILE --dry-run -r$ROOT_DIR -d0`)
-    # @test true
+    @info("Dry Run, -deep = 0")
+    run(`$JULIA_CMD --project=$PROJ_DIR -- $SCRIPT_FILE --dry-run -r$ROOT_DIR -d0`)
+    @test true
     
-    # println("\n" ^ 3)
-    # @info("Dry Run, -deep = 10")
-    # run(`$JULIA_CMD --project=$PROJ_DIR -- $SCRIPT_FILE --dry-run -r$ROOT_DIR -d10`)
-    # @test true
+    println("\n" ^ 3)
+    @info("Dry Run, -deep = 10")
+    run(`$JULIA_CMD --project=$PROJ_DIR -- $SCRIPT_FILE --dry-run -r$ROOT_DIR -d10`)
+    @test true
     
-    # println("\n" ^ 3)
-    # @info("Real Run, -deep = 0")
-    # run(`$JULIA_CMD --project=$PROJ_DIR -- $SCRIPT_FILE -r$ROOT_DIR -d0`)
-    # @test true
+    println("\n" ^ 3)
+    @info("Real Run, -deep = 0")
+    run(`$JULIA_CMD --project=$PROJ_DIR -- $SCRIPT_FILE -r$ROOT_DIR -d0`)
+    @test true
     
-    # println("\n" ^ 3)
-    # @info("Real Run, -deep = 3")
-    # run(`$JULIA_CMD --project=$PROJ_DIR -- $SCRIPT_FILE -r$ROOT_DIR -d3`)
-    # @test true
+    println("\n" ^ 3)
+    @info("Real Run, -deep = 3")
+    run(`$JULIA_CMD --project=$PROJ_DIR -- $SCRIPT_FILE -r$ROOT_DIR -d3`)
+    @test true
 
 end
